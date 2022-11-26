a9493de8edb23a52cbc1be0dc6214930d8b9cca8
rohanpatankar <rohanpatankar926@gmail.com>
2022-11-26 19:13:27 +0530

    gpt3 sampleFlask

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+from api import api
+app = Flask(__name__)
+
+openai.api_key = api.API_KEY
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=generate_prompt(animal),
+            temperature=0.6,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
+if __name__=="__main__":
+    app.run()
